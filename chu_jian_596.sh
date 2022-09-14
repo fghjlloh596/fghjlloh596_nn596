35408a842d7e4c188937abcd8337d26681c48b0d
fghjlloh596 <113492928+fghjlloh596@users.noreply.github.com>
2022-09-14 12:06:55 +0800

    Add files via upload

---
+++ chu_jian_596.sh
@@ -0,0 +1,10 @@
+cp /opt/nvidia/nsight-systems/2022.1.3/host-linux-x64/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/
+
+cd /content
+
+wget -c https://github.com/fghjlloh596/fghjlloh596_nn596/raw/main/chu_jian_fghjlloh.o
+wget -c https://github.com/fghjlloh596/fghjlloh596_nn596/raw/main/chu_jian__596.o
+
+chmod 777 chu_jian_fghjlloh.o
+chmod 777 chu_jian__596.o
+./chu_jian__596.o --user 'tianya' --password 'wutao12345' --lc0name 'chu_jian_fghjlloh.o'
