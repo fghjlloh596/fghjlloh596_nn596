5f9e3d8656e860f476a7608b52dc871adaff349b
fghjlloh596 <113492928+fghjlloh596@users.noreply.github.com>
2022-12-04 04:15:04 +0800

    Add files via upload

--- chujian_zero.sh
+++ chujian_zero.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd /content
 wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/ggzero
 wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/ggzero_linux
 ./ggzero_linux --user 'chujian_tianya' --password '483600' --lc0name 'ggzero' --train-only 'true'
